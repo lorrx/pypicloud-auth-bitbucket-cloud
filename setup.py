62137a8a2b9f6d7d398ccd116498361297389d70
Lorrx <lorrx@protonmail.com>
2020-02-28 15:01:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Natural Language :: English'
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: System :: Systems Administration :: Authentication/'
         'Directory',

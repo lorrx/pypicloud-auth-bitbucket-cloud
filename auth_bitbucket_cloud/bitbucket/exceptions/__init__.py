28e12eb7ba36dd3b37937f9b0df02b0a3b187980
Lorrx <lorrx@protonmail.com>
2020-03-06 17:26:45 +0100

    Add exceptions

---
+++ auth_bitbucket_cloud/bitbucket/exceptions/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package init file.
+"""
+from .authentication_errors import AuthenticationTypeNotImplementedError
+from .authentication_errors import AuthenticationUserAttributeError
---
+++ auth_bitbucket_cloud/bitbucket/exceptions/authentication_errors.py
@@ -0,0 +1,17 @@
+"""
+Collection of exception definitions in authentication scope.
+"""
+
+
+class AuthenticationTypeNotImplementedError(NotImplementedError):
+    """
+    AuthenticationTypeNotImplementedError class definition.
+    """
+    pass
+
+
+class AuthenticationUserAttributeError(AttributeError):
+    """
+    AuthenticationUserAttributeError
+    """
+    pass

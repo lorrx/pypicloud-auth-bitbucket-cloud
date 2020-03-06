5f932810fa7167e636042d119cb80fc19b467968
Lorrx <lorrx@protonmail.com>
2020-03-06 17:27:23 +0100

    Improve PEP8

--- auth_bitbucket_cloud/__init__.py
+++ auth_bitbucket_cloud/__init__.py
@@ -3,5 +3,6 @@ Package init file.
 """
 import os
 import sys
+from .bitbucket_cloud_access import BitbucketCloudAccess
 
 sys.path.insert(0, os.path.dirname(__file__))
---
+++ auth_bitbucket_cloud/bitbucket/__init__.py
@@ -0,0 +1,4 @@
+"""
+Package init file.
+"""
+from .bitbucket import Bitbucket
